05966bd3d1d27e82f78d52639d2ed4c55135555e
Conner McCabe <conner.p.mccabe@gmail.com>
2020-04-03 11:31:07 -0500

    adding geolocation services

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
+import Map from '../components/Map'
 
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                test
+                <Map />
+                
             </div>
         )
     }

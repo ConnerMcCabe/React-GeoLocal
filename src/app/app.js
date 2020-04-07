dae2c7986e1eea3bc22af5e004116776bc702494
Conner McCabe <conner.p.mccabe@gmail.com>
2020-04-07 13:07:39 -0500

    accessing lat/ lng

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './app.css';
-import Map from '../components/Map'
+import Map from '../components/Map';
+import { getCurrentLatLng } from '../services/geolocation';
 
 
 class App extends Component {
 
+    async componentDidMount() {
+        const {lat, lng} = await getCurrentLatLng();
+        console.log(lat, lng);
+    }
 
     render() {
         return (
